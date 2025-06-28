fd69cdb675296d9dd536af1b17d50cbb6065de65
tramos <70649355+tatimramos@users.noreply.github.com>
2025-06-28 16:50:03 -0300

    Add files via upload

---
+++ obesity_streamlit01.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import streamlit as st
+
+st.set_page_config(page_title="Estudo Obesidade",
+                   page_icon=":bar_chart:",
+                   layout="wide")
+
+st.header("**Estudo Obesidade**")
+
+df = pd.read_csv("Obesity.csv")
+
+st.bar_chart(df.Age)
